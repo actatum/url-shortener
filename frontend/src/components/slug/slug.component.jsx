d8dc0332643198c89b90f4550cc7b415120a14ba
Aaron Tatum <actatum@umich.edu>
2020-10-06 00:09:18 -0400

    point to api url and not localhost

--- frontend/src/components/slug/slug.component.jsx
+++ frontend/src/components/slug/slug.component.jsx
@@ -4,34 +4,34 @@ import { Link } from "react-router-dom"
 
 class Slug extends React.Component {
 
-    state = {
-        url: '',
-        errorMessage: ''
-    }
+  state = {
+    url: '',
+    errorMessage: ''
+  }
 
-    async componentDidMount() {
-        try {
-            const slug = this.props.location.pathname.slice(1)
-            const resp = await axios.get(`http://localhost:8080/url/${slug}`)
-            this.setState({ url: resp.data.url })
-        } catch (err) {
-            this.setState({ errorMessage: err.response.data.message })
+  async componentDidMount() {
+    try {
+      const slug = this.props.location.pathname.slice(1)
+      const resp = await axios.get(`${process.env.REACT_APP_API_URL}/url/${slug}`)
+      this.setState({ url: resp.data.url })
+    } catch (err) {
+      this.setState({ errorMessage: err.response.data.message })
+    }
+  }
+
+  render() {
+    const { url, errorMessage } = this.state
+    return (
+      <div>
+        {url !== '' ? (window.location = url) :
+          <div>
+            <h3>{errorMessage}</h3>
+            <Link to="/"><button>Go back</button></Link>
+          </div>
         }
-    }
-
-    render() {
-        const { url, errorMessage } = this.state
-        return (
-            <div>
-                {url !== '' ? (window.location = url) :
-                    <div>
-                        <h3>{errorMessage}</h3>
-                        <Link to="/"><button>Go back</button></Link>
-                    </div>
-                }
-            </div>
-        )
-    }
+      </div>
+    )
+  }
 }
 
 export default Slug
